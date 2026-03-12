056e119fce6176dd070002d6ab19186a4ebfb223
ARTHUR RODRIGUES GALVEZ SOUZA PINTO <arthur.pinto7@senaisp.edu.br>
2026-03-12 09:24:12 -0300

    Exercício 3 finalizado

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let btnAdicionar = document.querySelector('#btnEditar');
-let mensagem = document.querySelector('#mensagem');
- 
-btnEditar.addEventListener('click', ()=>{
-    mensagem.innerText = `Texto alterado com JavaScript!`
+let btnAdicionar = document.querySelector('#btnAdicionar');
+let inputTarefa = document.querySelector('#inputTarefa');
+let listaTarefas = document.querySelector('#listaTarefas');
+
+
+btnAdicionar.addEventListener('click', () => {
+    const texto = inputTarefa.value;
+    if (texto.trim() !== '' || texto.trim() !== undefined) {
+        const novaTarefa = document.createElement('li');
+        novaTarefa.textContent = texto;
+        listaTarefas.appendChild(novaTarefa);
+        inputTarefa.innerText = "";
+        inputTarefa.value = '';
+    } else {
+        alert('Por favor, digite algum texto!')
+    }
 })
