2dc30ee5c4851016c6997ce9df6f00939ccbf638
ARTHUR RODRIGUES GALVEZ SOUZA PINTO <arthur.pinto7@senaisp.edu.br>
2026-03-12 09:39:37 -0300

    Exercício 4 finalizado

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let btnAdicionar = document.querySelector('#btnAdicionar');
 let inputTarefa = document.querySelector('#inputTarefa');
 let listaTarefas = document.querySelector('#listaTarefas');
+let mensagem = document.querySelector("#mensagem");
 
 
 btnAdicionar.addEventListener('click', () => {
-    const texto = inputTarefa.value;
-    if (texto.trim() !== '' || texto.trim() !== undefined) {
+    let texto = inputTarefa.value;
+
+    if (texto.trim() !== '') {
         const novaTarefa = document.createElement('li');
         novaTarefa.textContent = texto;
         listaTarefas.appendChild(novaTarefa);
-        inputTarefa.innerText = "";
         inputTarefa.value = '';
+
+        mensagem.innerText = 'Tarefa adicionada com sucesso!'
+        mensagem.className = 'fw-bold text-success';
     } else {
-        alert('Por favor, digite algum texto!')
+        mensagem.innerText = 'Por favor, digite algum texto!'
+        mensagem.className ='fw-bold text-danger';
     }
 })
