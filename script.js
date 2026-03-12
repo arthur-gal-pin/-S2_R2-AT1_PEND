780d1796d2852efa4b04de3354e45850ad65ab5f
ARTHUR RODRIGUES GALVEZ SOUZA PINTO <arthur.pinto7@senaisp.edu.br>
2026-03-12 10:17:31 -0300

    Exercício 5 finalizado.

--- script.js
+++ script.js
@@ -8,15 +8,36 @@ btnAdicionar.addEventListener('click', () => {
     let texto = inputTarefa.value;
 
     if (texto.trim() !== '') {
+        const unidade = document.createElement('div');
+        unidade.className = 'row';
+        listaTarefas.appendChild(unidade);
+
         const novaTarefa = document.createElement('li');
+        novaTarefa.id = 'tarefa';
         novaTarefa.textContent = texto;
-        listaTarefas.appendChild(novaTarefa);
+        novaTarefa.className = 'list-group-item col-8'
+        
+        const botaoExclusao = document.createElement('button');
+        botaoExclusao.innerText = 'Excluir';
+        botaoExclusao.classList = 'btn btn-danger ms-3 col-3';
+        novaTarefa.appendChild(botaoExclusao);
+
+        unidade.appendChild(novaTarefa);
+        
+        botaoExclusao.addEventListener('click', () => {
+            botaoExclusao.parentElement.remove();
+            mensagem.innerText = 'Tarefa excluída com sucesso!';
+            mensagem.className = 'fw-bold text-warning';
+        })
+
         inputTarefa.value = '';
 
         mensagem.innerText = 'Tarefa adicionada com sucesso!'
         mensagem.className = 'fw-bold text-success';
     } else {
         mensagem.innerText = 'Por favor, digite algum texto!'
-        mensagem.className ='fw-bold text-danger';
+        mensagem.className = 'fw-bold text-danger';
     }
 })
+
+
