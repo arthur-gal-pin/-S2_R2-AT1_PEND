6210c7506a64d60f036b032bdd85e56ffeb8f5ba
ARTHUR RODRIGUES GALVEZ SOUZA PINTO <arthur.pinto7@senaisp.edu.br>
2026-03-12 10:49:36 -0300

    Mudanças de design

--- script.js
+++ script.js
@@ -10,25 +10,27 @@ btnAdicionar.addEventListener('click', () => {
     if (texto.trim() !== '') {
         const unidade = document.createElement('div');
         unidade.className = 'row';
-        listaTarefas.appendChild(unidade);
 
         const novaTarefa = document.createElement('li');
         novaTarefa.id = 'tarefa';
         novaTarefa.textContent = texto;
         novaTarefa.className = 'list-group-item col-8'
-        
+
         const botaoExclusao = document.createElement('button');
         botaoExclusao.innerText = 'Excluir';
         botaoExclusao.classList = 'btn btn-danger ms-3 col-3';
-        novaTarefa.appendChild(botaoExclusao);
 
-        unidade.appendChild(novaTarefa);
-        
+
         botaoExclusao.addEventListener('click', () => {
             botaoExclusao.parentElement.remove();
             mensagem.innerText = 'Tarefa excluída com sucesso!';
             mensagem.className = 'fw-bold text-warning';
         })
+
+        novaTarefa.appendChild(botaoExclusao);
+        unidade.appendChild(novaTarefa);
+        listaTarefas.appendChild(unidade);
+
 
         inputTarefa.value = '';
 
